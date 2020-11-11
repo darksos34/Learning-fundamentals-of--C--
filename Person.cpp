6b39e73f95ea7f0091a2de381f897648085f2cd8
Your Name <j.hamwijk34@gmail.com>
2020-11-11 21:26:27 +0100

    Plurasight Demo Learning

---
+++ Person.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <iomanip>
+
+#define ArrayLength(x) (sizeof((x)) / sizeof((x)[0]))
+
+class Person
+{
+    int age;
+    char name[25];
+}; 
+
--- main.cpp
+++ main.cpp
@@ -1,30 +1,45 @@
 #include <iostream>
+#include "Person.cpp"
 
-//This is a single line comment. Comments are ignored by the compiler
 
-/*
-    Multi - line comment. 
-    This type of comment can span multiple lines
-*/
+enum Role
+{
+    Role_Visitor,
+    Role_Doctor,
+    Role_Patient,
+    Role_Nurse,
+    Maximum_Role
+};
+
 int main(int argc, char **argv)
 {
-    int age; 
-    age = 20; 
-    
-    float height = 1.8f;
+    std::cout << "Switch statement example" << std::endl << std::endl;
 
-    char singleChar = 'A';
-    unsigned char smallNumber = 128; 
-
-    std::cout << "Hello world." << std::endl;
-
-    //Check the size in bytes of some basic data types
-    std::cout << "Size of char " << sizeof(char) << std::endl;
-    std::cout << "Size of int " << sizeof(int) <<  std::endl;
-    std::cout << "Size of unsigned int " << sizeof(unsigned int) <<  std::endl;
-    std::cout << "Size of short int " << sizeof(short int) <<  std::endl;
-    std::cout << "Size of float " << sizeof(float) <<  std::endl;
-    std::cout << "Size of double " << sizeof(double) <<  std::endl;
+    for (int index = 0; index < Maximum_Role; ++index)
+    {
+        switch(index)
+        {
+        case Role_Doctor:
+            {
+                std::cout << "I am a doctor. I treat patients." << std::endl;
+                break;
+            }
+        case Role_Nurse:
+            {
+                std::cout << "I am a nurse. I treat patients." << std::endl;
+                break;
+            }
+        case Role_Patient:
+            {
+                std::cout << "I am a patient. I have come for treatment." << std::endl;
+               
+            }
+        default:
+            {
+                std::cout << "I am just visiting." << std::endl;
+            }
+        }
+    }
 
     return 0;
 }
